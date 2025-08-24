12ae6979c2b9e4228c0b5eb5ad7bb537570bdc1c
digitalnarad <digitalnarad.tech@gmail.com>
2025-08-24 18:42:37 +0530

    Remove try catch

--- src/modules/category/controller.js
+++ src/modules/category/controller.js
@@ -12,91 +12,72 @@ import {
     updateOne,
 } from "../../config/db.service.js";
 import { modelName } from "../../utils/helper.js";
+import { asyncHandler } from "../../utils/asyncHandler.js";
 
 // Create a new category
-export const createCategory = async (req, res) => {
-    try {
-        const category = await createOne(modelName.CATEGORY, req.body);
-        return response201(res, "Category created successfully", category);
-    } catch (err) {
-        return response400(res, err.message);
-    }
-};
+export const createCategory = asyncHandler(async (req, res) => {
+  const category = await createOne(modelName.CATEGORY, req.body);
+  return response201(res, "Category created successfully", category);
+});
 
 // Get all categories
-export const getAllCategories = async (req, res) => {
-    try {
-        const categories = await findAll(
-            modelName.CATEGORY,
-            { isDeleted: false },
-            {},
-            { lean: true }
-        );
-        return response200(res, "Categories fetched successfully", categories);
-    } catch (err) {
-        return response500(res, err.message);
-    }
-};
+export const getAllCategories = asyncHandler(async (req, res) => {
+  const categories = await findAll(
+    modelName.CATEGORY,
+    { isDeleted: false },
+    {},
+    { lean: true }
+  );
+  return response200(res, "Categories fetched successfully", categories);
+});
 
 // Get a single category by ID
-export const getCategoryById = async (req, res) => {
-    try {
-        const category = await findOne(
-            modelName.CATEGORY,
-            { _id: req.params.id },
-            {},
-            { lean: true }
-        );
+export const getCategoryById = asyncHandler(async (req, res) => {
+  const category = await findOne(
+    modelName.CATEGORY,
+    { _id: req.params.id },
+    {},
+    { lean: true }
+  );
 
-        if (!category || category.isDeleted) {
-            return response404(res, "Category not found");
-        }
+  if (!category || category.isDeleted) {
+    return response404(res, "Category not found");
+  }
 
-        return response200(res, "Category fetched successfully", category);
-    } catch (err) {
-        return response500(res, err.message);
-    }
-};
+  return response200(res, "Category fetched successfully", category);
+});
 
 // Update a category
-export const updateCategory = async (req, res) => {
-    try {
-        const category = await updateOne(
-            modelName.CATEGORY,
-            { _id: req.params.id },
-            req.body,
-            { runValidators: true }
-        );
+export const updateCategory = asyncHandler(async (req, res) => {
+  const category = await updateOne(
+    modelName.CATEGORY,
+    { _id: req.params.id },
+    req.body,
+    { runValidators: true }
+  );
 
-        if (!category) {
-            return response404(res, "Category not found");
-        }
+  if (!category) {
+    return response404(res, "Category not found");
+  }
 
-        return response200(res, "Category updated successfully", category);
-    } catch (err) {
-        return response400(res, err.message);
-    }
-};
+  return response200(res, "Category updated successfully", category);
+});
 
 // Soft delete a category
-export const deleteCategory = async (req, res) => {
-    try {
-        const category = await findOne(
-            modelName.CATEGORY,
-            { _id: req.params.id },
-            {},
-            { lean: false }
-        );
+export const deleteCategory = asyncHandler(async (req, res) => {
+  const category = await findOne(
+    modelName.CATEGORY,
+    { _id: req.params.id },
+    {},
+    { lean: false }
+  );
 
-        if (!category || category.isDeleted) {
-            return response404(res, "Category not found");
-        }
+  if (!category || category.isDeleted) {
+    return response404(res, "Category not found");
+  }
 
-        category.isDeleted = true;
-        await category.save();
+  category.isDeleted = true;
+  await category.save();
 
-        return response200(res, "Category deleted successfully");
-    } catch (err) {
-        return response500(res, err.message);
-    }
-};
+  return response200(res, "Category deleted successfully");
+});
